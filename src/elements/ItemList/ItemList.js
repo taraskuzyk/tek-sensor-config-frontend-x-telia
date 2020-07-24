6347652adb8437317f766fa6ceac44c0c4bdaf2c
taraskuzyk <tkuzyk@tektelic.com>
2020-07-24 10:47:23 -0600

    > better handling of incomming live uplinks

--- src/elements/ItemList/ItemList.js
+++ src/elements/ItemList/ItemList.js
@@ -1,14 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {ListGroup, ListGroupItem} from "shards-react";
 
 export default function ItemList({items, onClick, getItemLabel}){
 
     const [activeItem, setActiveItem] = useState(items ? (items[0] ? items[0] : null) : null);
+
+    useEffect(()=> {
+        setActiveItem((oldItems)=> {
+            if (oldItems[0] !== items[1])
+                // prevents from annoying refresh when only one item is added to the list
+                // e.g. if we have
+                //                  [item7, item8, item9]
+                // and we change to
+                //                  [item6, item7, item8, item9]
+                // while we have item9 selected, the user does't get "kicked" to item6, but stays at item9.
+                setActiveItem(items[0])
+        })
+    }, [items])
     //
     if (items) {
         return (
             <div>
-            <ListGroup >
+            <ListGroup>
                 {
                     items.map((item, i) => {
                         return (
